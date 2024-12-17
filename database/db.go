a857d3135d33cec85c2f2eeecd346002bb4af325
matheushasm <matheushasm@icloud.com>
2024-12-17 00:06:20 +0000

    Loading db credentials from .env file

--- database/db.go
+++ database/db.go
@@ -2,17 +2,29 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func Connect() {
-	var err error
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error on load .env file")
+	}
 
-	dsn := "root:password@tcp(127.0.0.1:3306)/my_database"
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, database)
 
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
